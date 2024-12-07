ae5309b9d9eea282e50c432b1cfebdba2c44abad
Samandar Azamov <162783893+JscorpTech@users.noreply.github.com>
2024-12-07 16:38:51 +0500

    Update apps.py

--- apps.py
+++ apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ModuleConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "core.apps.{{module_name}}"
+    name = "{{module_name}}"
